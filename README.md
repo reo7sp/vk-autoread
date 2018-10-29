a8daa3b9f018ec68432a46a70ee959da8ce177aa
Oleg Morozenkov <m@oleg.rocks>
2018-10-29 12:19:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# VK autoread
+
+Автоматически помечает беседы прочитанными, чтобы не маячил гребанный кружочек с циферкой.
+
+## Конфиг
+
+Файл с конфигом должен быть в рабочей папке под названием `config.yml`.
+
+Пример конфига есть в файле `config.yml.template`.
+
+Описание конфига:
+```
+vk_token: str                     # access-токен вк
+autoread_all_muted: bool          # автоматически отмечать прочитанными все чаты
+always_autoread_chats: List[str]  # список названий бесед, которые всегда нужно отмечать прочитанными
+deny_autoread_chats: List[str]    # список названий бесед, которые никогда не нужно отмечать прочитанными
+```
+
+## Access-токен ВК
+
+Как получить токен:
+
+1. Создать приложение на https://vk.com/apps?act=manage
+2. Заменить в такой ссылке номер приложения:
+    ```
+    https://oauth.vk.com/authorize?client_id=<НОМЕР ПРИЛОЖЕНИЯ>&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=offline,messages&response_type=token&v=5.85
+    ```
+3. Перейти по ссылке, разрешить права, потом, скопировать из адресной строки access_token.
+
+## Запуск
+
+```sh
+pip3 install -r requirement.txt  # чтобы установить зависимости
+
+python3 ./app.py
+```
+
---
+++ app.py
@@ -0,0 +1,88 @@
+import logging
+from datetime import timedelta
+from time import sleep
+
+import yaml
+from vk_api import VkApi
+
+_logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    # read config
+    with open('config.yml') as f:
+        config = yaml.load(f)
+
+    # check config
+    if 'vk_token' in config:
+        if not isinstance(config['vk_token'], str):
+            raise ValueError(f'invalid vk_token in config, '
+                             f'must be string, got {type(config["vk_token"])}')
+    else:
+        raise ValueError('required vk_token in config')
+
+    if 'autoread_all_muted' in config and config['autoread_all_muted'] is not None:
+        if not isinstance(config['autoread_all_muted'], bool):
+            raise ValueError(f'invalid autoread_all_muted in config, '
+                             f'must be bool, got {type(config["autoread_all_muted"])}')
+    else:
+        config['autoread_all_muted'] = False
+
+    if 'always_autoread_chats' in config and config['always_autoread_chats'] is not None:
+        if not isinstance(config['always_autoread_chats'], list):
+            raise ValueError(f'invalid always_autoread_chats in config, '
+                             f'must be list of strings, got {type(config["always_autoread_chats"])}')
+        if len(config['always_autoread_chats']) != 0:
+            if not all(isinstance(it, str) for it in config['always_autoread_chats']):
+                raise ValueError('invalid always_autoread_chats in config, '
+                                 'must be list of strings')
+    else:
+        config['always_autoread_chats'] = list()
+
+    if 'deny_autoread_chats' in config and config['deny_autoread_chats'] is not None:
+        if not isinstance(config['deny_autoread_chats'], list):
+            raise ValueError(f'invalid deny_autoread_chats in config, '
+                             f'must be list of strings, got {type(config["deny_autoread_chats"])}')
+        if len(config['deny_autoread_chats']) != 0:
+            if not all(isinstance(it, str) for it in config['deny_autoread_chats']):
+                raise ValueError('invalid deny_autoread_chats in config, '
+                                 'must be list of strings')
+    else:
+        config['deny_autoread_chats'] = list()
+
+    # setup vk
+    vk_session = VkApi(token=config['vk_token'])
+    vk = vk_session.get_api()
+
+    # start daemon
+    while True:
+        dialogs = vk.messages.getConversations(filter='unread', count=5)
+        dialogs = dialogs['items']
+
+        for it in dialogs:
+            it = it['conversation']
+
+            if 'chat_settings' not in it:
+                continue
+
+            if 'title' not in it['chat_settings']:
+                continue
+
+            peed_id = it['peer']['id']
+            title = it['chat_settings']['title']
+            is_muted = it.get('push_settings', dict()).get('no_sound', False)
+
+            do_autoread = False
+            if config['autoread_all_muted'] and is_muted:
+                do_autoread = True
+            if any(title.lower() in config_title.lower() for config_title in config['always_autoread_chats']):
+                do_autoread = True
+            if any(title.lower() in config_title.lower() for config_title in config['deny_autoread_chats']):
+                do_autoread = False
+
+            if do_autoread:
+                _logger.info(f'Marking as read chat with title {title}')
+                vk.messages.markAsRead(peer_id=peed_id)
+
+        sleep(timedelta(seconds=5).total_seconds())
